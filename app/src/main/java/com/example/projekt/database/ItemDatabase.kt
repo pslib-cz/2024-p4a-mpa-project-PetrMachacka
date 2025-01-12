9158260fc8cfa78ca46455c0583991da0d4966ec
Petr Machačka <91247837+PetrMachacka@users.noreply.github.com>
2025-01-12 20:33:12 +0100

    hotovo

--- app/src/main/java/com/example/projekt/database/ItemDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.projekt.database
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Item::class], version = 1, exportSchema = false)
-abstract class ItemDatabase: RoomDatabase() {
-    abstract fun itemDao(): ItemDao
-}
